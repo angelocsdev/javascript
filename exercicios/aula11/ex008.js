f5e6d82a5fa9178c481e6021ae974237958f7690
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2022-12-21 14:45:42 -0200

    aula 11

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,9 @@
+//console.log('O console funcionou corretamente!') // Use para executar um comando no node.js/console.
+
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60){ // Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'BR'
+console.log(`Se você vive em ${país}.`)
+if (país == 'BR') {
+    console.log(' Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+} // Condição com if e else.
