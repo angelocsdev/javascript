7fb6714e4ddb60788121e0df9a5903b460a2c008
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2022-12-23 19:37:57 -0200

    aula 14

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,25 @@
+/*
+
+Repetição normal
+
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+*/
+
+// Repetição com FOR
+/*
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+*/
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+// Para abrir depuração no VS Code clique em 'Executar' e iniciar depuração ou aperte 'F5'
