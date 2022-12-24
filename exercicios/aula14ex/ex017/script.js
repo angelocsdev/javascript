2bd8045bf9ebf9d40fcc403e68174880330eee5c
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2022-12-23 23:06:15 -0200

    exercicio

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    if (num.value.length == 0) {
+        alert(`Por favor digite um número!`)
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')//forma de criar elementos em JS.
+        }
+    }
+    
+}
