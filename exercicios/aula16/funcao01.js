f3e2c534a070fbbfbad99056c525549162dce853
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2022-12-25 12:08:13 -0200

    função 1

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,18 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'ímpar'
+    }
+}
+
+let res = parimpar(2212392)
+console.log(res)
+
+/*
+ ou
+
+
+console.log(parimpar(2212392))
+
+*/
