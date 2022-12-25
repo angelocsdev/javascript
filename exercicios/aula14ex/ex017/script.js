fe1046c887358fbddfea4ce2e50e87c4be38fa48
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2022-12-25 09:30:22 -0200

    aula 14 atualização

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -6,9 +6,13 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')//forma de criar elementos em JS.
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
     }
-    
 }
