d6b63849dcb108ae70da1c31c436ae1df98a6eef
Angelo <angelobielcosta123@gmail.com>
2025-05-17 19:36:49 -0300

    Aula 2

---
+++ javascript-devaprender/aula02/variaveis.js
@@ -0,0 +1,26 @@
+// let idade = 5
+// console.log(idade)
+// let altura = 100
+// console.log(altura)
+
+/*
+
+Como declarar variáveis?
+
+- camelCase
+- Não podem ter espaços
+- Não podem começar com números
+
+*/
+
+let nome = 'Duda'
+
+let idade = 20
+
+let altura = 1.67
+
+let graduacao = 'Recursos Humanos'
+
+let hobbies = ['dançar', 'cantar', 'jogar videogame']
+
+console.log(nome, idade, altura, graduacao, hobbies)
