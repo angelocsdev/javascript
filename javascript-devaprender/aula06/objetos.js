12bb1ee59ae1bd1383c1d7ab46b34d7db5d2434c
Angelo <angelobielcosta123@gmail.com>
2025-05-18 11:08:36 -0300

    Aula 06

---
+++ javascript-devaprender/aula06/objetos.js
@@ -0,0 +1,21 @@
+let nome = 'Gabriel'
+let idade = 22
+let estaAprovado = true
+let sobrenome
+let corSelecionada = null
+let notaMedia = 7.5
+let hobbie = 'cantar'
+let faculdade = 'Análise e Desenvolvimento de Sistemas'
+
+let pessoa = {
+    nome: 'Gabriel',
+    idade: 22,
+    estaAprovado: true,
+    sobrenome: 'Gabriel',
+    corSelecionada: 'Azul',
+    notaMedia: 7.5,
+    hobbie,
+    faculdade
+}
+
+console.log(pessoa)
