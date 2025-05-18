9be0ea8f49497e3df0fc334825e949cc2526b5d8
Angelo <angelobielcosta123@gmail.com>
2025-05-18 11:05:31 -0300

    Aula 03

---
+++ javascript-devaprender/aula03/constantes.js
@@ -0,0 +1,9 @@
+/*
+
+Com a variável let você pode trocar o valor da variável sempre que quiser, mas com constantes isso não é possível.
+
+*/
+
+const valorIngressoAdulto = 20 // uma boa prática é usar o const como tipo de variável e só use let quando for alterar valores
+// valorIngressoAdulto = 30 
+console.log(valorIngressoAdulto)
