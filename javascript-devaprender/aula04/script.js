b2863d37398c1214ccc63764baf41df7bdf4599e
Angelo <angelobielcosta123@gmail.com>
2025-05-18 11:06:31 -0300

    Aula 04

---
+++ javascript-devaprender/aula04/script.js
@@ -0,0 +1,5 @@
+let nome = 'Gabriel' // string literal
+let idade = 22 // number literal
+let estaAprovado = true // boolean
+let sobrenome // Undefined
+let corSelecionada = null // Redefinir um valor
