693300d0ca68e8bfc013a6f6ceee123e8fd87ddf
Angelo <angelobielcosta123@gmail.com>
2025-05-19 14:45:44 -0300

    Aula 12

---
+++ javascript-devaprender/aula12/op-atribuicao.js
@@ -0,0 +1,6 @@
+// Operadores de Atribuição
+
+let valorTecladoGamer = 100 // atribue um valor a algo
+valorTecladoGamer += valorTecladoGamer
+valorTecladoGamer -= valorTecladoGamer
+console.log(valorTecladoGamer)
