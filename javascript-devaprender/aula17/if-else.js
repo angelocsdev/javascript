8e7fdb835d5766e213c42a2ca80064eb3914c7ff
Angelo <angelobielcosta123@gmail.com>
2025-05-19 16:03:40 -0300

    Aula 17

---
+++ javascript-devaprender/aula17/if-else.js
@@ -0,0 +1,22 @@
+// IF ELSE
+
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até 18:00 : Boa tarde!
+// Caso contrário: Boa noite!
+
+let hora = 10
+
+if(hora > 6 && hora < 12) {
+    // código a ser executado
+    console.log("bom dia")
+}
+
+else if (hora > 12 && hora < 18) {
+    // código a ser executado
+    console.log("boa tarde")
+}
+
+else {
+    // código a ser executado
+    console.log("boa noite")
+}
