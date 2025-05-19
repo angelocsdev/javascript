186d9c24c452eaea21f0cedcd4fd404ddddb219f
Angelo <angelobielcosta123@gmail.com>
2025-05-19 14:45:25 -0300

    Aula 11

---
+++ javascript-devaprender/aula11/op-aritmeticos.js
@@ -0,0 +1,19 @@
+// Operadores Aritméticos (Matemáticos)
+
+let salario = 1000
+
+// + , - , * , / , ** (exponencial)
+
+console.log(salario + salario)
+console.log(salario - salario)
+console.log(salario * salario)
+console.log(salario / salario)
+console.log(salario ** salario)
+
+// ++ --
+
+let idade = 18
+console.log(idade++) // se colocar o ++ depois ele só aparece depois de outro console.log
+console.log(++idade) // antes ele aparece junto
+console.log(idade--)
+console.log(idade)
