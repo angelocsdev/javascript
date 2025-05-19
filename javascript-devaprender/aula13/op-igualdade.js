506e6a4f2905da276e8d26ea5c0b2f6e7f0847f9
Angelo <angelobielcosta123@gmail.com>
2025-05-19 14:46:02 -0300

    Aula 13

---
+++ javascript-devaprender/aula13/op-igualdade.js
@@ -0,0 +1,13 @@
+// Operadores de Igualdade
+
+// Igualdade estrita
+
+// True
+console.log(1 === 1) // tem 3 "=" porque está comparando os valores e o tipo
+// False
+console.log('1' === 1)
+
+// Igualdade solta
+
+console.log(1 == 1) // true
+console.log('1' == 1) // true
