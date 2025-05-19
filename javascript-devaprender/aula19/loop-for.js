cb08114bddb95bb034c17b82e6191f4a3fd67375
Angelo <angelobielcosta123@gmail.com>
2025-05-19 16:04:52 -0300

    Aula 19

---
+++ javascript-devaprender/aula19/loop-for.js
@@ -0,0 +1,24 @@
+// Maneira errada
+
+// console.log('Estou aprendendo!')
+// console.log('Estou aprendendo!')
+// console.log('Estou aprendendo!')
+// console.log('Estou aprendendo!')
+// console.log('Estou aprendendo!')
+
+// Maneira correta
+
+// 1 - For
+
+for(i = 1; i <= 5; i++) {
+    if(i % 2 !== 0) {
+        console.log(i)
+    }
+    
+}
+
+// 2 - while
+// 3 - Do - while
+// 4 - for - in
+// 5 - for - of
+
