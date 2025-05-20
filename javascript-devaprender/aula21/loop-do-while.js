af58c25d1f234e9e165cd4c2de55cbcc6a593308
Angelo <angelobielcosta123@gmail.com>
2025-05-20 10:07:06 -0300

    Aula 21

---
+++ javascript-devaprender/aula21/loop-do-while.js
@@ -0,0 +1,9 @@
+// Laço Do While
+let i = 0
+do { // faça isso v
+//                v
+//                v
+//                v
+    console.log("Digitando!")
+    i++
+} while (i < 10) // enquanto...
