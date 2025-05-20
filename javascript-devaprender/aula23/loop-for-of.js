3eb1024e8037f3e6a33452433b61a922eaa34ac6
Angelo <angelobielcosta123@gmail.com>
2025-05-20 10:20:35 -0300

    Aula 23

---
+++ javascript-devaprender/aula23/loop-for-of.js
@@ -0,0 +1,13 @@
+// Laço for-of
+
+const cores = ['vermelho', 'azul', 'verde']
+
+for(let cor of cores) {
+    console.log(cor)
+}
+
+const idades = [12, 22, 8, 15, 45, 56, 72, 31]
+
+for(let idade of idades) {
+    console.log(idade)
+}
