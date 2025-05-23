362584302098e9ffbdc6db54f185ce23a80b45de
Angelo <angelobielcosta123@gmail.com>
2025-05-23 09:44:48 -0300

    Mini Projeto 09

---
+++ javascript-devaprender/mini-projetos/09-mini-projeto/scripts.js
@@ -0,0 +1,19 @@
+// Criar uma função que exibe a quantidade de * que aquela linha possui
+
+exibirAsteriscos(5)
+
+function exibirAsteriscos(linhas) {
+    // let padrao = ''
+    // for(let linha = 1; linha <= linhas; linha++) {
+    //     padrao += '*'
+    //     console.log(padrao)
+    // }
+
+    for(let linha = 1; linha <= linhas; linha++) {
+        let padrao = ''
+        for(let i = 0; 0< linha; i++) {
+            padrao += '*'
+        }
+        console.log(padrao)
+    }
+}
