5bab63bc8c6f5465d145e1a524c4e305a63ed8f1
Angelo <angelobielcosta123@gmail.com>
2025-05-31 15:48:10 -0300

    Aula 29

---
+++ javascript-devaprender/aula29/scripts.js
@@ -0,0 +1,17 @@
+// Template Literal
+
+const msg = "Olá, Mundo!"
+
+// Object {}
+// Boolean true,false
+// string '',""
+// template ``
+
+let sinal = "!"
+
+
+const newMsg = `Olá, 'Mundo'${sinal}
+
+MY NEW SHOW!`
+
+console.log(newMsg)
