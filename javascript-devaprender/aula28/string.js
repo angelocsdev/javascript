21309193d186161c8684d0d1fa1690f9d9c10a92
Angelo <angelobielcosta123@gmail.com>
2025-05-31 15:34:38 -0300

    Aula 28

---
+++ javascript-devaprender/aula28/string.js
@@ -0,0 +1,13 @@
+// Tipo primitivo
+
+const msg = "Hello, World!"
+
+// Tipo objeto
+
+const outraMsg = new String('very good')
+
+console.log(outraMsg.length)
+console.log(outraMsg.includes('very good'))
+console.log(msg.startsWith("Hello"))
+console.log(msg.endsWith("jjj"))
+console.log(msg.indexOf("Hello"))
