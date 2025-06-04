ec93bca69d4ee1ba83755e3793ee5704eada1713
Angelo Silva <angelobielcosta123@gmail.com>
2025-06-04 15:00:35 -0300

    Aula 30

---
+++ javascript-devaprender/aula30/date.js
@@ -0,0 +1,7 @@
+// Date
+const data1 = new Date()
+const data2 = new Date('Juny 06 2025 14:57')
+const data3 = new Date('2025,06,04,14,58')
+
+date3.setFullYear(2030)
+console.log(data1,data2,data3)
