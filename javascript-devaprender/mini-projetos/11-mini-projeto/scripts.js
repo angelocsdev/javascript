81d7d0b05f8a0c841dda752f711b45d5901b54d7
Angelo Silva <angelobielcosta123@gmail.com>
2025-06-04 15:05:32 -0300

    Mini Projeto 11

---
+++ javascript-devaprender/mini-projetos/11-mini-projeto/scripts.js
@@ -0,0 +1,20 @@
+// Objeto Endereço
+
+// Criar objeto endereço que contém:
+// Rua
+// Cidade
+// CEP
+// exibirEndereco(endereco)
+
+let endereco = {
+    rua: 'a',
+    cidade: 'b',
+    cep: 'c'
+}
+
+function exibirEndereco(endereco) {
+    for(let chave in endereco)
+        console.log(chave,endereco[chave])
+}
+
+exibirEndereco(endereco)
