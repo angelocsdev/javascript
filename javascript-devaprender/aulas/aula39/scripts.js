68791eadabf6b78b426a9b4a42ef086d669423d2
Angelo Silva <angelobielcosta123@gmail.com>
2025-06-12 12:33:28 -0300

    Aula 39

---
+++ javascript-devaprender/aulas/aula39/scripts.js
@@ -0,0 +1,30 @@
+// Combinando e cortando arrays
+
+const primeiro = [1,2,3]
+const segundo = [4,5,6]
+
+// Combinar
+
+const combinado = primeiro.concat(segundo)
+
+console.log(combinado)
+
+// Dividir
+
+const cortado = combinado.slice(1,2)
+console.log(cortado)
+
+// exercicio
+
+const um = ['a','b','c']
+const dois = ['d','e','f']
+
+// adicionando
+
+const adc = um.concat(dois)
+console.log(adc)
+
+// dividindo
+
+const dividindo = adc.slice(1,3)
+console.log(dividindo)
