50b0360dbcd8e93932bfabbaf0cc95471a9b4e0f
Angelo Silva <angelobielcosta123@gmail.com>
2025-06-12 12:46:32 -0300

    Aula 41

---
+++ javascript-devaprender/aulas/aula41/scripts.js
@@ -0,0 +1,9 @@
+// iterando um array
+
+const numeros = [1,2,3,4,5,6]
+//foreach
+
+numeros.forEach((numero,i) => console.log(numero,i))
+
+const letras = ['a','b','c']
+letras.forEach((letra,e) => console.log(letra,e))
